d1c518abd75b1422c938ca1ca149d8437c7fae68
xy <71631080+iisskj@users.noreply.github.com>
2025-07-29 19:44:56 +0800

    Create config.py
    support Chinese.

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# 服务器的 IP 地址（必须是本机局域网 IP，如 192.168.1.100）
+# 在运行 server.py 的那台电脑上执行：ipconfig（Windows）或 ifconfig（Mac/Linux）查看
+SERVER_IP = "10.113.211.166"  # ❗每台电脑都要改成服务器的真实 IP
+
+PORT = 5000
